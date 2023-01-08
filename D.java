75abc5ebf108bf503b00abd85d6a36d8bcb935c2
Anaveen61 <76729081+Anaveen61@users.noreply.github.com>
2023-01-08 21:08:14 +0530

    Add files via upload
    for a new commit changes

---
+++ D.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;  
+  
+  
+public class D {  
+  
+    public static void main(String[] args) {  
+  
+        Scanner scanner= new Scanner(System.in);  
+Public static sum(float f1,float f2)
+{
+    this.f1=f1;
+    this.f2=f2;
+        System.out.print("Enter first number: ");  
+  
+        Float f1=scanner.nextFloat();  
+  
+        System.out.print("Enter second number: ");  
+  
+        Float f2=scanner.nextFloat();  
+  
+        Float f3=Float.sum(f1,f2);  
+  
+        System.out.print("Enter third number: ");  
+  
+        Float f4=scanner.nextFloat();  
+  
+        System.out.println("Sum = "+Float.sum(f3,f4));  
+}
+  
+    }  
+  
+}  
